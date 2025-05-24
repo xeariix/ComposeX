c78771990cb8e745cbb98e3503e6de69deb51fa1
Kevin Breinert <98883117+kevinbreinert@users.noreply.github.com>
2025-05-24 02:46:30 +0200

    Add custom selectable icon chip

---
+++ app/src/main/java/com/xeariix/composex/components/SelectableIconChip.kt
@@ -0,0 +1,137 @@
+package com.xeariix.composex.components
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.xeariix.composex.sizes.Paddings
+
+/**
+ * A highly customizable icon chip component that displays a label and optionally an icon when selected.
+ *
+ * This chip can be used in filters, selection groups, or settings where visual feedback is required upon interaction.
+ * The appearance of the chip dynamically changes based on its clicked state, including background, text, and icon color.
+ *
+ * @param name the text displayed inside the chip.
+ * @param imageVector optional [ImageVector] to display as an icon when the chip is selected.
+ * @param modifier optional [Modifier] to be applied to the chip.
+ * @param containerColor the background [Color] of the chip in its default (unclicked) state.
+ * @param iconColor the [Color] applied to the icon when visible.
+ * @param textColor the [Color] applied to the label text in the default state.
+ * @param shape the [Shape] defining the chip's outline.
+ * @param elevation the [CardElevation] controlling the elevation of the chip in various states.
+ * @param border optional [BorderStroke] to draw around the chip’s container.
+ * @param isClicked indicates whether the chip is currently selected, triggering visual changes and optional icon visibility.
+ */
+@Composable
+fun SelectableIconChip(
+    name: String,
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector? = null,
+    containerColor: Color = MaterialTheme.colorScheme.onTertiary,
+    iconColor: Color = MaterialTheme.colorScheme.onSurface,
+    textColor: Color = MaterialTheme.colorScheme.primary,
+    shape: Shape = CardDefaults.shape,
+    elevation: CardElevation = CardDefaults.cardElevation(),
+    border: BorderStroke? = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.primary),
+    isClicked: Boolean = false,
+) {
+    val animatedContainerColor by animateColorAsState(
+        targetValue = if (!isClicked) containerColor else MaterialTheme.colorScheme.primary,
+        animationSpec = tween(durationMillis = 300), // oder spring() für eine "bounce"-artige Animation
+        label = "ContainerColorAnimation"
+    )
+
+    Card(
+        modifier = modifier,
+        shape = shape,
+        colors = CardDefaults.cardColors(containerColor = animatedContainerColor),
+        elevation = elevation,
+        border = border,
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    horizontal = 15.dp,
+                    vertical = 6.dp,
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            if (imageVector != null) {
+                AnimatedContent(
+                    targetState = isClicked,
+                    transitionSpec = {
+                        (fadeIn() + scaleIn()).togetherWith(fadeOut() + scaleOut())
+                    },
+                    label = "IconAnimation",
+                ) { targetState ->
+                    if (targetState) {
+                        Icon(
+                            imageVector = imageVector,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(22.dp)
+                                .padding(
+                                    top = Paddings.empty,
+                                    bottom = Paddings.empty,
+                                    end = Paddings.small,
+                                ),
+                            tint = if (!isClicked) iconColor else Color.White,
+                        )
+                    }
+                }
+            }
+
+            Text(
+                text = name,
+                fontSize = 19.sp,
+                style = TextStyle(color = if (!isClicked) textColor else Color.White)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun IconChipPreview() {
+    SelectableIconChip(
+        name = "Filter",
+    )
+}
+
+@Preview
+@Composable
+fun IconChipIsClickedPreview() {
+    SelectableIconChip(
+        name = "Filter",
+        imageVector = Icons.Default.Done,
+        isClicked = true,
+    )
+}
---
+++ app/src/main/java/com/xeariix/composex/sizes/Paddings.kt
@@ -0,0 +1,12 @@
+package com.xeariix.composex.sizes
+
+import androidx.compose.ui.unit.dp
+
+object Paddings {
+    val empty = 0.dp
+    val extraSmall = 2.dp
+    val small = 6.dp
+    val medium = 12.dp
+    val large = 15.dp
+    val extraLarge = 20.dp
+}
