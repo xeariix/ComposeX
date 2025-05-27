7dd42e933fc67bf94c9de19d04651b4002323464
Kevin Breinert <98883117+kevinbreinert@users.noreply.github.com>
2025-05-27 20:53:47 +0200

    (WIP) Add ui test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.junit.ktx)
+    implementation(libs.androidx.ui.test.junit4.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +58,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.test.manifest)
 }
---
+++ app/src/test/java/com/xeariix/composex/components/SelectableIconChipKtTest.kt
@@ -0,0 +1,14 @@
+package com.xeariix.composex.components
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SelectableIconChipKtTest {
+
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+}
